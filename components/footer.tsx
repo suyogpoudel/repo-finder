f336e266864431670e0fe2e7f1dae4ce53c1f33a
Suyog Poudel <suyogpoudel583@gmail.com>
2026-02-19 18:25:49 +0545

    added footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,21 @@
-// TODO: Create a footer
+import Link from "next/link";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="w-full bg-card p-6 border-t shadow-sm flex justify-between items-center text-muted-foreground">
+      <span>&copy; 2026 | Suyog Poudel</span>
+
+      <span className="flex gap-1">
+        Developed by:
+        <Link
+          href="https://suyogpoudel.com.np"
+          className="text-primary hover:underline underline-offset-4"
+        >
+          Suyog Poudel
+        </Link>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
